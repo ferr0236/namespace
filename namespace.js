2ab1539724e05f3c8e0a8dafd30f092092e8e5bb
Clauber Ferreira <ferr0236@algonquinlive.com>
2018-10-23 10:05:37 -0400

    Assignment implementation

---
+++ namespace.js
@@ -0,0 +1,23 @@
+var FERR0236 = function init() {
+	let div = document.createElement("div");
+	div.setAttribute("className", "box");
+	div.textContent = "ferr0236";
+	
+	div.addEventListener("mouseover", () => {
+		div.classList.toggle("highlight");
+	});
+	
+	div.addEventListener("mouseout", () => {
+		div.classList.toggle("highlight");
+	});
+	
+	div.addEventListener("click", () => {
+		div.style.borderColor = "yellow";
+		div.style.backgroundColor = "green";
+	});
+	
+	let boxes = document.querySelector("boxes");
+	if (boxes) {
+		boxes.append(div);
+	}
+};
