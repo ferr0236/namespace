59817eaf1c85e69fef05b0cd5a0bd87c116bafd8
Clauber Ferreira <ferr0236@algonquinlive.com>
2018-10-23 14:34:59 -0400

    bug fix

--- namespace.js
+++ namespace.js
@@ -1,23 +1,25 @@
-var FERR0236 = function init() {
-	let div = document.createElement("div");
-	div.setAttribute("class", "box");
-	div.textContent = "ferr0236";
-	
-	div.addEventListener("mouseover", () => {
-		div.classList.toggle("highlight");
-	});
-	
-	div.addEventListener("mouseout", () => {
-		div.classList.toggle("highlight");
-	});
-	
-	div.addEventListener("click", () => {
-		div.style.borderColor = "yellow";
-		div.style.backgroundColor = "green";
-	});
-	
-	let boxes = document.querySelector("#boxes");
-	if (boxes) {
-		boxes.append(div);
+var FERR0236 = {
+	function init() {
+		let div = document.createElement("div");
+		div.setAttribute("class", "box");
+		div.textContent = "ferr0236";
+
+		div.addEventListener("mouseover", () => {
+			div.classList.toggle("highlight");
+		});
+
+		div.addEventListener("mouseout", () => {
+			div.classList.toggle("highlight");
+		});
+
+		div.addEventListener("click", () => {
+			div.style.borderColor = "yellow";
+			div.style.backgroundColor = "green";
+		});
+
+		let boxes = document.querySelector("#boxes");
+		if (boxes) {
+			boxes.append(div);
+		}
 	}
 }
